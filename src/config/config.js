f05ff9620c02c268b6751dae84018e00867b5d09
sudhakar <sudhakar@hubino.com>
2026-06-16 12:32:51 +0530

    deployment

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "Sudhakar@106",
+    database: "heal_point",
+    host: "localhost",
+    dialect: "mysql",
+  },
+
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+  },
+};
